7c1b55946ff392f1a4bbe60e7dd43491242ce598
Chris Thompson <g0kla@arrl.net>
2017-12-22 21:58:16 -0500

    Updated server version info to reflec the change to WOD table layout

--- src/FoxTelemServer.java
+++ src/FoxTelemServer.java
@@ -42,7 +42,7 @@ import common.Log;
 
 public class FoxTelemServer {
 
-	public static String version = "Version 0.25 - 20 Dec 2017";
+	public static String version = "Version 0.26 - 22 Dec 2017";
 	public static int port = Config.tcpPort;
 	static int sequence = 0;
 	private static final int MAX_SEQUENCE = 1000;// This needs to be larger than the maximum number of connections in a second so we dont get duplicate file names
