e41db733f495cbeea7b1a9072df40cd7c179c1aa
Chris Thompson <g0kla@arrl.net>
2018-01-12 19:24:35 -0500

    New version info for v1.06h

--- src/common/Config.java
+++ src/common/Config.java
@@ -61,8 +61,8 @@ public class Config {
 	
 	public static ProgressPanel fileProgress;
 	
-	public static String VERSION_NUM = "1.06g";
-	public static String VERSION = VERSION_NUM + " - 11 Jan 2018";
+	public static String VERSION_NUM = "1.06h";
+	public static String VERSION = VERSION_NUM + " - 12 Jan 2018";
 	public static final String propertiesFileName = "FoxTelem.properties";
 	
 	public static final String WINDOWS = "win";
