411f60c7bfdeaa699554dc80d6c0ab26c239d7ab
Chris Thompson <g0kla@arrl.net>
2018-03-11 08:00:14 +0900

    Initial files for FCDP implementation of LibUSB

--- src/device/fcd/FCD1TunerController.java
+++ src/device/fcd/FCD1TunerController.java
@@ -17,33 +17,23 @@
  ******************************************************************************/
 package device.fcd;
 
+import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 
 import javax.usb.UsbClaimException;
 import javax.usb.UsbException;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.usb4java.Device;
 import org.usb4java.DeviceDescriptor;
 
-import source.SourceException;
-import source.tuner.MixerTunerType;
-import source.tuner.TunerClass;
-import source.tuner.TunerType;
-import source.tuner.configuration.TunerConfiguration;
-import source.tuner.fcd.FCDCommand;
-import source.tuner.fcd.FCDTunerController;
+import device.DeviceException;
+import device.DevicePanel;
 
 public class FCD1TunerController extends FCDTunerController
 {
-	private final static Logger mLog = 
-			LoggerFactory.getLogger( FCD1TunerController.class );
-
-	public static final int sMINIMUM_TUNABLE_FREQUENCY = 64000000;
-	public static final int sMAXIMUM_TUNABLE_FREQUENCY = 1700000000;
-	public static final int sSAMPLE_RATE = 96000;
+	public static final int MINIMUM_TUNABLE_FREQUENCY = 64000000;
+	public static final int MAXIMUM_TUNABLE_FREQUENCY = 1700000000;
+	public static final int SAMPLE_RATE = 96000;
 	
 	private double mDCCorrectionInPhase = 0.0;
 	private double mDCCorrectionQuadrature = 0.0;
@@ -52,32 +42,209 @@ public class FCD1TunerController extends FCDTunerController
 	private LNAGain mLNAGain;
 	private LNAEnhance mLNAEnhance;
 	private MixerGain mMixerGain;
-	private FCD1TunerConfiguration mTunerConfiguration;
-	private FCD1TunerEditor mEditor;
+	
+	public static String[] lnaGain = {"-5dB","-2.5dB","0dB","2.5dB","5dB","7.5dB","10dB","12.5dB","15dB","17.5dB","20dB","25dB","30dB"};
 
-	public FCD1TunerController( Device device, DeviceDescriptor descriptor ) 
+	// TUNER_MIXER_GAIN_ENUM
+	public static String[] mixerGain = {"4dB", "12dB"};
+	int TMGE_P4_0DB=0,
+	 TMGE_P12_0DB=1;
+	
+	// TUNER_LNA_ENHANCE_ENUM
+	int TLEE_OFF=0,
+			  TLEE_0=1,
+			  TLEE_1=3,
+			  TLEE_2=5,
+			  TLEE_3=7;
+	
+	// TUNER_BAND_ENUM
+	int TBE_VHF2=0,
+	  TBE_VHF3=1,
+	  TBE_UHF=2, 
+	  TBE_LBAND=3;
+	
+	String[] band = {"VHF II", "VHF III", "UHF", "LBAND"};
+	
+	String[] vhfIIFilterName = {"268MHz", "299MHz"};
+	String[] vhfIIIFilterName = {"509MHz", "656MHz"};
+	String[] uhfFilterName = {"360MHz", "380MHz","405MHz", "425MHz", "450MHz", "475MHz", "505MHz", "540MHz", "575MHz", "615MHz", 
+			"670MHz", "720MHz", "760MHz", "840MHz", "890MHz", "970MHz"};
+	String[] lbandFilterName = {"1300MHz", "1320MHz", "1360MHz","1410MHz","1445MHz","1460MHz","1490MHz","1530MHz","1560MHz",
+			"1590MHz","1640MHz","1660MHz","1680MHz","1700MHz","1720MHz","1750MHz"};
+	
+	// TUNER_RF_FILTER_ENUM
+	int
+	// Band 0, VHF II
+	  TRFE_LPF268MHZ=0,
+	  TRFE_LPF299MHZ=8,
+	  // Band 1, VHF III
+	  TRFE_LPF509MHZ=0,
+	  TRFE_LPF656MHZ=8,
+	  // Band 2, UHF
+	  TRFE_BPF360MHZ=0,
+	  TRFE_BPF380MHZ=1,
+	  TRFE_BPF405MHZ=2,
+	  TRFE_BPF425MHZ=3,
+	  TRFE_BPF450MHZ=4,
+	  TRFE_BPF475MHZ=5,
+	  TRFE_BPF505MHZ=6,
+	  TRFE_BPF540MHZ=7,
+	  TRFE_BPF575MHZ=8,
+	  TRFE_BPF615MHZ=9,
+	  TRFE_BPF670MHZ=10,
+	  TRFE_BPF720MHZ=11,
+	  TRFE_BPF760MHZ=12,
+	  TRFE_BPF840MHZ=13,
+	  TRFE_BPF890MHZ=14,
+	  TRFE_BPF970MHZ=15,
+	  // Band 2, L band
+	  TRFE_BPF1300MHZ=0,
+	  TRFE_BPF1320MHZ=1,
+	  TRFE_BPF1360MHZ=2,
+	  TRFE_BPF1410MHZ=3,
+	  TRFE_BPF1445MHZ=4,
+	  TRFE_BPF1460MHZ=5,
+	  TRFE_BPF1490MHZ=6,
+	  TRFE_BPF1530MHZ=7,
+	  TRFE_BPF1560MHZ=8,
+	  TRFE_BPF1590MHZ=9,
+	  TRFE_BPF1640MHZ=10,
+	  TRFE_BPF1660MHZ=11,
+	  TRFE_BPF1680MHZ=12,
+	  TRFE_BPF1700MHZ=13,
+	  TRFE_BPF1720MHZ=14,
+	  TRFE_BPF1750MHZ=15;
+	  
+	
+	
+	// TUNER_BIAS_CURRENT_ENUM
+	int  TBCE_LBAND=0,
+			  TBCE_1=1,
+			  TBCE_2=2,
+			  TBCE_VUBAND=3;
+	
+	// TUNER_MIXER_FILTER_ENUM
+	int  TMFE_27_0MHZ=0,
+			  TMFE_4_6MHZ=8,
+			  TMFE_4_2MHZ=9,
+			  TMFE_3_8MHZ=10,
+			  TMFE_3_4MHZ=11,
+			  TMFE_3_0MHZ=12,
+			  TMFE_2_7MHZ=13,
+			  TMFE_2_3MHZ=14,
+			  TMFE_1_9MHZ=15;
+	
+	// TUNER_IF_GAIN1_ENUM
+	int  TIG1E_N3_0DB=0,
+			  TIG1E_P6_0DB=1;
+	
+	// TUNER_IF_GAIN_MODE_ENUM
+	int  TIGME_LINEARITY=0,
+			  TIGME_SENSITIVITY=1;
+	
+	// TUNER_IF_RC_FILTER_ENUM
+	int TIRFE_21_4MHZ=0,
+			  TIRFE_21_0MHZ=1,
+			  TIRFE_17_6MHZ=2,
+			  TIRFE_14_7MHZ=3,
+			  TIRFE_12_4MHZ=4,
+			  TIRFE_10_6MHZ=5,
+			  TIRFE_9_0MHZ=6,
+			  TIRFE_7_7MHZ=7,
+			  TIRFE_6_4MHZ=8,
+			  TIRFE_5_3MHZ=9,
+			  TIRFE_4_4MHZ=10,
+			  TIRFE_3_4MHZ=11,
+			  TIRFE_2_6MHZ=12,
+			  TIRFE_1_8MHZ=13,
+			  TIRFE_1_2MHZ=14,
+			  TIRFE_1_0MHZ=15;
+	
+	// TUNER_IF_GAIN2_ENUM
+	int TIG2E_P0_0DB=0,
+			  TIG2E_P3_0DB=1,
+			  TIG2E_P6_0DB=2,
+			  TIG2E_P9_0DB=3;
+	
+	// TUNER_IF_GAIN3_ENUM
+	int TIG3E_P0_0DB=0,
+			  TIG3E_P3_0DB=1,
+			  TIG3E_P6_0DB=2,
+			  TIG3E_P9_0DB=3;
+	
+	// TUNER_IF_GAIN4_ENUM
+	int  TIG4E_P0_0DB=0,
+			  TIG4E_P1_0DB=1,
+			  TIG4E_P2_0DB=2;
+	
+	// TUNER_IF_FILTER_ENUM
+	int  TIFE_5_50MHZ=0,
+			  TIFE_5_30MHZ=1,
+			  TIFE_5_00MHZ=2,
+			  TIFE_4_80MHZ=3,
+			  TIFE_4_60MHZ=4,
+			  TIFE_4_40MHZ=5,
+			  TIFE_4_30MHZ=6,
+			  TIFE_4_10MHZ=7,
+			  TIFE_3_90MHZ=8,
+			  TIFE_3_80MHZ=9,
+			  TIFE_3_70MHZ=10,
+			  TIFE_3_60MHZ=11,
+			  TIFE_3_40MHZ=12,
+			  TIFE_3_30MHZ=13,
+			  TIFE_3_20MHZ=14,
+			  TIFE_3_10MHZ=15,
+			  TIFE_3_00MHZ=16,
+			  TIFE_2_95MHZ=17,
+			  TIFE_2_90MHZ=18,
+			  TIFE_2_80MHZ=19,
+			  TIFE_2_75MHZ=20,
+			  TIFE_2_70MHZ=21,
+			  TIFE_2_60MHZ=22,
+			  TIFE_2_55MHZ=23,
+			  TIFE_2_50MHZ=24,
+			  TIFE_2_45MHZ=25,
+			  TIFE_2_40MHZ=26,
+			  TIFE_2_30MHZ=27,
+			  TIFE_2_28MHZ=28,
+			  TIFE_2_24MHZ=29,
+			  TIFE_2_20MHZ=30,
+			  TIFE_2_15MHZ=31;
+	
+	// TUNER_IF_GAIN5_ENUM
+	int TIG5E_P3_0DB=0,
+			  TIG5E_P6_0DB=1,
+			  TIG5E_P9_0DB=2,
+			  TIG5E_P12_0DB=3,
+			  TIG5E_P15_0DB=4;
+	
+	// TUNER_IF_GAIN6_ENUM
+	int   TIG6E_P3_0DB=0,
+			  TIG6E_P6_0DB=1,
+			  TIG6E_P9_0DB=2,
+			  TIG6E_P12_0DB=3,
+			  TIG6E_P15_0DB=4;
+
+	public FCD1TunerController( Device device, DeviceDescriptor descriptor ) throws DeviceException 
 	{
-		super( device, 
-			   descriptor,
-			   (int)MixerTunerType.FUNCUBE_DONGLE_PRO.getAudioFormat().getSampleRate(),
-			   sMINIMUM_TUNABLE_FREQUENCY, 
-			   sMAXIMUM_TUNABLE_FREQUENCY );
+		super( device, descriptor, SAMPLE_RATE,
+				   MINIMUM_TUNABLE_FREQUENCY, MAXIMUM_TUNABLE_FREQUENCY );
 	}
 	
-	public void init() throws SourceException
+	public void init() throws DeviceException
 	{
 		super.init();
 		
-		mFrequencyController.setSampleRate( sSAMPLE_RATE );
+		//mFrequencyController.setSampleRate( SAMPLE_RATE );
 		
 		try
 		{
 			setFCDMode( Mode.APPLICATION );
-			getPhaseAndGainCorrection();
-			getDCIQCorrection();
-			getLNAGainSetting();
-			getLNAEnhanceSetting();
-			getMixerGainSetting();
+			//getPhaseAndGainCorrection();
+			//getDCIQCorrection();
+			//getLNAGainSetting();
+			//getLNAEnhanceSetting();
+			//getMixerGainSetting();
 			
 			send( FCDCommand.APP_SET_MIXER_GAIN, 1l );
 		}
@@ -85,75 +252,16 @@ public class FCD1TunerController extends FCDTunerController
 		{
 			e.printStackTrace();
 			
-			throw new SourceException( "FCDTunerController error " +
-					"during construction", e );
+			throw new DeviceException( "FCDTunerController error " +
+					"during construction: " + e.getMessage() );
 		}
 	}
 	
 	public int getCurrentSampleRate()
 	{
-		return sSAMPLE_RATE;
-	}
-
-	@Override
-    public TunerClass getTunerClass()
-    {
-	    return TunerClass.FUNCUBE_DONGLE_PRO;
-    }
-
-	@Override
-    public TunerType getTunerType()
-    {
-	    return TunerType.FUNCUBE_DONGLE_PRO;
-    }
-
-	/**
-	 * Get the tuner configuration
-	 * 
-	 * @return current config or null if one hasn't been applied yet
-	 */
-	public FCD1TunerConfiguration getTunerConfiguration()
-	{
-		return mTunerConfiguration;
+		return SAMPLE_RATE;
 	}
 	
-	@Override
-    public void apply( TunerConfiguration tunerConfig )
-            throws SourceException
-    {
-		FCD1TunerConfiguration config = (FCD1TunerConfiguration)tunerConfig;
-		
-		try
-		{
-			setFrequencyCorrection( config.getFrequencyCorrection() );
-			setLNAGain( config.getLNAGain() );
-			setLNAEnhance( config.getLNAEnhance() );
-			setMixerGain( config.getMixerGain() );
-			setDCCorrectionInPhase( config.getInphaseDCCorrection() );
-			setDCCorrectionQuadrature( config.getQuadratureDCCorrection() );
-			setGainCorrection( config.getGainCorrection() );
-			setPhaseCorrection( config.getPhaseCorrection() );
-			
-			//If we get to here without exception, store this config
-			mTunerConfiguration = config;
-		}
-		catch( Exception e)
-		{
-			//Re-cast any usb exceptions as source exceptions
-			throw new SourceException( "FCD 1 Controller encountered an "
-					+ "exception while applying tuner config - " + 
-					e.getLocalizedMessage() );
-		}
-		
-		try
-		{
-			setFrequency( config.getFrequency() );
-		}
-		catch( SourceException se )
-		{
-			//Do nothing, we couldn't set the frequency
-		}
-	}
 
 	/**
 	 * Gets the current LNA Gain value from the controller and stores it
@@ -716,4 +824,9 @@ public class FCD1TunerController extends FCDTunerController
     		return getLabel();
     	}
     }
+
+	@Override
+	public DevicePanel getDevicePanel() throws IOException, DeviceException {
+		return new FcdProPanel();
+	}
 }
---
+++ src/device/fcd/FcdProPanel.java
@@ -0,0 +1,196 @@
+package device.fcd;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.io.IOException;
+
+import javax.swing.BoxLayout;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.TitledBorder;
+
+import common.Log;
+import device.TunerController;
+import device.DeviceException;
+import device.DevicePanel;
+
+@SuppressWarnings("serial")
+public class FcdProPanel extends DevicePanel implements ItemListener, ActionListener, Runnable {
+	int NUM_OF_PARAMS = 15;
+	boolean running = true;
+	boolean done = false;
+	FCD1TunerController fcd;
+	@SuppressWarnings("rawtypes")
+	JComboBox cbMixerGain;
+	@SuppressWarnings("rawtypes")
+	JComboBox cbLnaGain;
+	JTextField rfFilterValue;
+	JTextField bandValue;
+	JTextField ifFilterValue;
+	
+	public FcdProPanel() throws IOException, DeviceException {
+		TitledBorder title = new TitledBorder(null, "Funcube Dongle Pro", TitledBorder.LEADING, TitledBorder.TOP, null, null);
+		//title.setTitleFont(new Font("SansSerif", Font.PLAIN, 12));
+		this.setBorder(title);
+		initializeGui();
+	}
+	
+	public void setEnabled(boolean b) {
+		cbMixerGain.setEnabled(b);
+		cbLnaGain.setEnabled(b);		
+	}
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public void initializeGui() throws IOException, DeviceException {
+		setLayout(new BorderLayout(3,3));
+		JPanel center = new JPanel();
+		add(center, BorderLayout.NORTH);
+		center.setLayout(new BoxLayout(center, BoxLayout.X_AXIS));
+		
+		// LNA gain 10 default, -5 to +30 
+		JLabel lblLna = new JLabel("LNA Gain");
+		center.add(lblLna);
+		cbLnaGain = new JComboBox(FCD1TunerController.lnaGain);
+		center.add(cbLnaGain);
+		cbLnaGain.addItemListener(this);
+		
+		// Mixer Gain 4dB or 12dB
+		JLabel lblMix = new JLabel("    Mixer Gain");
+		center.add(lblMix);
+		cbMixerGain = new JComboBox(FCD1TunerController.mixerGain);
+		
+		center.add(cbMixerGain);
+		cbMixerGain.addItemListener(this);
+		
+		// IF Gain 1 - -3dB or +6dB
+		
+		// Band
+		JLabel lblband = new JLabel("    Band");
+		center.add(lblband);
+		bandValue = new JTextField();
+		bandValue.setEnabled(false);
+		center.add(bandValue);
+		
+		// RF Filter - not needed 268Mhz vs 298Mhz
+		JLabel rfFilter = new JLabel("    RF Filter");
+		center.add(rfFilter);
+		rfFilterValue = new JTextField();
+		rfFilterValue.setEnabled(false);
+		center.add(rfFilterValue);
+
+
+	}
+	
+	@Override
+	public void setDevice(TunerController fcd) throws IOException, DeviceException {
+		setFcd((FCD1TunerController)fcd);
+		
+	}
+	public void setFcd(FCD1TunerController f) throws IOException, DeviceException { 
+		fcd = f; 
+		getSettings();
+	}
+	
+	public void updateFilter() throws IOException, DeviceException {
+		rfFilterValue.setText(fcd.getRfFilter());
+		bandValue.setText(fcd.getBand());
+	}
+	
+	public void getSettings()  throws IOException, DeviceException {
+		try {
+			Thread.sleep(100);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}  // Allow startup to settle down first
+		boolean mixer = fcd.getMixerGain();
+		if (mixer)
+			cbMixerGain.setSelectedIndex(1);
+		else 
+			cbMixerGain.setSelectedIndex(0);
+		cbLnaGain.setSelectedIndex(fcd.getLnaGain());	
+		rfFilterValue.setText(fcd.getRfFilter());
+		bandValue.setText(fcd.getBand());
+//		ifFilterValue.setText(fcd.getIfFilter());
+	}
+	
+	
+	@Override
+	public void run() {
+		done = false;
+		running = true;
+
+		while(running) {
+
+			try {
+				Thread.sleep(1000); // approx 1 sec refresh
+			} catch (InterruptedException e) {
+				Log.println("ERROR: FCD thread interrupted");
+				//e.printStackTrace();
+			} 
+
+
+			if (fcd != null) {
+				try {
+					getSettings();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (DeviceException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+				this.repaint();
+			}
+		}			
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+	}
+
+	@Override
+	public void itemStateChanged(ItemEvent e) {
+		if (e.getSource() == cbMixerGain) {
+			try {
+				int position = cbMixerGain.getSelectedIndex();
+				if (position == 1)
+					fcd.setMixerGain(true);
+				else 
+					fcd.setMixerGain(false);
+			} catch (DeviceException e1) {
+				Log.println("Error setting LNA Gain on FCD");
+				e1.printStackTrace(Log.getWriter());
+			} 
+		}
+		if (e.getSource() == cbLnaGain) {
+			try {
+				int position = cbLnaGain.getSelectedIndex();
+				fcd.setLnaGain(position);
+			} catch (DeviceException e1) {
+				Log.println("Error setting LNA Gain on FCD");
+				e1.printStackTrace(Log.getWriter());
+			} 
+		}
+		
+	}
+
+	@Override
+	public int getSampleRate() {
+		return fcd.SAMPLE_RATE;
+	}
+
+	@Override
+	public int getDecimationRate() {
+		return 1;
+	}
+
+	
+
+}
