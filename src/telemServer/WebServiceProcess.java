43249ee365e51e8ecedab102861ef2df481f96c8
Chris Thompson <g0kla@arrl.net>
2018-03-30 13:50:30 -0400

    Service version

--- src/telemServer/WebServiceProcess.java
+++ src/telemServer/WebServiceProcess.java
@@ -25,7 +25,7 @@ import common.Log;
 
 public class WebServiceProcess implements Runnable {
 	PayloadDbStore payloadDbStore;
-	public static String version = "Version 0.21 - 12 Feb 2017";
+	public static String version = "Version 0.22 - 30 Mar 2018";
 	private Socket socket = null;
 	int port = 8080;
 	
