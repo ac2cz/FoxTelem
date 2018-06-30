7f0d9f04fef57769d77ab28c08f060eed82b4a35
Chris Thompson <g0kla@arrl.net>
2018-06-29 20:00:12 -0400

    Decode the MODE bits in the new header for Husky.  Still need to save them to the payloads and display.

--- src/telemetry/FoxBPSK/FoxBPSKHeader.java
+++ src/telemetry/FoxBPSK/FoxBPSKHeader.java
@@ -1,10 +1,17 @@
 package telemetry.FoxBPSK;
 
 import common.Config;
+import common.Spacecraft;
 import decoder.FoxDecoder;
 import telemetry.Header;
 
 public class FoxBPSKHeader extends Header {
+	// Extended Mode Bits that are only in FoxId 6 and later
+	int safeMode;
+	int healthMode;
+	int scienceMode;
+	int cameraMode;
+	int minorVersion;
 	
 	public FoxBPSKHeader() {
 		super(TYPE_EXTENDED_HEADER);
@@ -22,8 +29,27 @@ public class FoxBPSKHeader extends Header {
 		type = nextbits(4);
 		if (id == 0) // then take the foxId from the next 8 bits
 			id = nextbits(8);
+		if (id >= Spacecraft.HUSKY_SAT) {
+			safeMode = nextbits(1);
+			healthMode = nextbits(1);
+			scienceMode = nextbits(1);
+			cameraMode = nextbits(1);
+			minorVersion = nextbits(4);
+
+		}
 	}
-	
+
+	public String getMode() {
+		if (id >= Spacecraft.HUSKY_SAT) {
+			if (safeMode != 0 ) return "SAFE";
+			if (healthMode != 0 ) return "HEALTH";
+			if (scienceMode != 0 ) return "SCIENCE";
+			if (cameraMode != 0 ) return "CAMERA";
+			return "UNKNOWN";
+		}
+		return null;
+	}
+
 	public boolean isValid() {
 		copyBitsToFields();
 		if (Config.satManager.validFoxId(id) && isValidType(type))
@@ -47,6 +73,9 @@ public class FoxBPSKHeader extends Header {
 				+ " RESET COUNT: " + FoxDecoder.dec(resets)
 				+ " UPTIME: " + FoxDecoder.dec(uptime)
 				+ " TYPE: " + FoxDecoder.dec(type);
+		
+		if (id >= Spacecraft.HUSKY_SAT)
+			s = s + " - " + getMode() + " MODE";
 		return s;
 	}
 }
