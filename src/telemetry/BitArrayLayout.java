2edc1b7cbb22248b66bb83b782cf771f0b048a17
Chris Thompson <g0kla@arrl.net>
2018-08-18 11:02:35 -0400

    Updated layouts with latest changed from Burns

--- src/telemetry/BitArrayLayout.java
+++ src/telemetry/BitArrayLayout.java
@@ -112,6 +112,7 @@ public class BitArrayLayout {
 	public static final int CONVERT_COM1_MAGNETOMETER = 46;
 	public static final int CONVERT_COM1_SPIN = 47;
 	public static final int CONVERT_COM1_GYRO_TEMP = 48;
+	public static final int CONVERT_HUSKY_ISIS_ANT_TEMP = 49;
 	
 	/**
 	 * Create an empty layout for manual init
