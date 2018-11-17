d474c793490b88e5add208dbdb4596d41fc5fd41
Chris Thompson <g0kla@arrl.net>
2018-11-17 13:23:27 -0500

    Version info 1.07t

--- src/common/Config.java
+++ src/common/Config.java
@@ -63,7 +63,7 @@ public class Config {
 	public static ProgressPanel fileProgress;
 	
 	public static String VERSION_NUM = "1.07t";
-	public static String VERSION = VERSION_NUM + " - 14 Nov 2018";
+	public static String VERSION = VERSION_NUM + " - 17 Nov 2018";
 	public static final String propertiesFileName = "FoxTelem.properties";
 	
 	public static final String WINDOWS = "win";
