0afc861d335f184819f2c6b1f81d2f0d3a346e29
Chris Thompson <g0kla@arrl.net>
2019-10-15 19:29:31 -0400

    Make sure Linux DIR Chooser is not Native. #587 In fact only MAC works.

--- src/gui/SettingsFrame.java
+++ src/gui/SettingsFrame.java
@@ -725,9 +725,8 @@ public class SettingsFrame extends JDialog implements ActionListener, ItemListen
 			if (!Config.logFileDirectory.equalsIgnoreCase("")) {
 				dir = new File(Config.logFileDirectory);
 			}
-			if(Config.useNativeFileChooser && !Config.isWindowsOs()) { // not on windows because native dir chooser does not work) {
-				// use the native file dialog on the mac by default
-				// by default Linux does not use the native fileChooser
+			if(Config.isMacOs()) { // not on Linx/windows because native dir chooser does not work) {
+				// use the native file DIR dialog on the mac by default
 				System.setProperty("apple.awt.fileDialogForDirectories", "true");
 				FileDialog fd =
 						new FileDialog(this, "Choose Directory for Log Files",FileDialog.LOAD);
