572b930a9db7f677686ecf4d3f1b27535c4197d7
Chris Thompson <g0kla@arrl.net>
2020-12-13 12:18:01 -0500

    #614 Switch modes and freq with Find Signal if in IQ mode and position calculated

--- src/common/PassManager.java
+++ src/common/PassManager.java
@@ -716,7 +716,7 @@ public class PassManager implements Runnable {
 							if (((Config.foxTelemCalcsPosition || Config.useDDEforAzEl) && !Config.findSignal) || Config.whenAboveHorizon) {
 								// We try to retune if FoxTelem Calc is on and FindSignal is not.
 								if (Config.retuneCenterFrequency) {
-									if (Config.foxTelemCalcsDoppler)
+									if (Config.iq)
 										retunedCenterFreqIfNeeded(sat);
 									switchedFormatIfNeeded(sat);
 								}
