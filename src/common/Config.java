f490d292c5ee1e1c2e6c58ad93f13ebf485d745a
Chris Thompson <g0kla@arrl.net>
2021-04-18 11:39:25 -0400

    Version info for 1.11 release

--- src/common/Config.java
+++ src/common/Config.java
@@ -66,8 +66,8 @@ public class Config {
 	
 	public static ProgressPanel fileProgress;
 	
-	public static String VERSION_NUM = "1.10d4";
-	public static String VERSION = VERSION_NUM + " - 18 Feb 2021";
+	public static String VERSION_NUM = "1.11 (Math)";
+	public static String VERSION = VERSION_NUM + " - 18 Apr 2021";
 	public static final String propertiesFileName = "FoxTelem.properties";
 	
 	public static final String WINDOWS = "win";
