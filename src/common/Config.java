f972c4232bc1ce0b340b478165086f6a76e43f45
Chris Thompson <g0kla@arrl.net>
2021-06-26 14:29:19 -0400

    Version info

--- src/common/Config.java
+++ src/common/Config.java
@@ -66,7 +66,7 @@ public class Config {
 	
 	public static ProgressPanel fileProgress;
 	
-	public static String VERSION_NUM = "1.11c";
+	public static String VERSION_NUM = "1.11b";
 	public static String VERSION = VERSION_NUM + " - 26 Jun 2021";
 	public static final String propertiesFileName = "FoxTelem.properties";
 	
