9f5ce451cda04890844f5c08b0a3e11a59fc63d6
Chris Thompson <g0kla@arrl.net>
2022-03-28 11:13:53 -0400

    Truncate labels that are too long on graphs

--- src/gui/graph/GraphCanvas.java
+++ src/gui/graph/GraphCanvas.java
@@ -432,6 +432,11 @@ public abstract class GraphCanvas extends MapPanel {
 							if (conversion != null && conversion instanceof ConversionStringLookUpTable) {
 								ConversionStringLookUpTable cslt = (ConversionStringLookUpTable)conversion;
 								s = cslt.calculateString(labels[v]-1); // we offset by 1 so the last label is not on the axis
+								if (s.length() > 9) {
+									// the label will not fit, truncate it
+									s = s.substring(0,9);
+								}
+									
 								if (s.equalsIgnoreCase(ConversionStringLookUpTable.ERROR))
 									drawLabel = false;
 								else
