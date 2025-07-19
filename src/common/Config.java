ae520f54434f4536be7e56b619a01432cd48d38d
Chris Thompson <g0kla@arrl.net>
2025-07-19 15:30:38 -0400

    Version infor for 1.13j

--- src/common/Config.java
+++ src/common/Config.java
@@ -67,8 +67,8 @@ public class Config {
 	
 	public static ProgressPanel fileProgress;
 	
-	public static String VERSION_NUM = "1.13i";
-	public static String VERSION = VERSION_NUM + " - 19 Jun 2025";
+	public static String VERSION_NUM = "1.13j";
+	public static String VERSION = VERSION_NUM + " - 19 Jul 2025";
 	public static String propertiesFileName = "FoxTelem.properties"; // this will be the name if setup() is not called with a different name
 	
 	public static final String WINDOWS = "win";
